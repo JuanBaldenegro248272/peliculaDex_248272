1ac8ca73c606919bdc452c8ca9aa1760b7ec83aa
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2026-02-17 19:28:51 -0700

    Add files via upload

---
+++ PracticaPelicula_248272/app/src/main/java/valenzuela/isabel/practicapelicula_253301/activity_login.kt
@@ -0,0 +1,27 @@
+package valenzuela.isabel.practicapelicula_253301
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val ingresar = findViewById<Button>(R.id.btnIngresar)
+        ingresar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
