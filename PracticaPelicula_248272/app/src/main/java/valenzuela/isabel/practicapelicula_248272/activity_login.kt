4a46e17775c7ac8534730c8b1f3297248f39a6c6
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2026-02-22 18:20:24 -0700

    fix
    Borrar el modo noche predeterminado

--- PracticaPelicula_248272/app/src/main/java/valenzuela/isabel/practicapelicula_248272/activity_login.kt
+++ PracticaPelicula_248272/app/src/main/java/valenzuela/isabel/practicapelicula_248272/activity_login.kt
@@ -11,7 +11,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class activity_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
